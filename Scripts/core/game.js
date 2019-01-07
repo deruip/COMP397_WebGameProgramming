5e881099b146c70357613966a28eae5b05b5e0ab
deruip <aronly1997@hotmail.com>
2019-01-07 18:18:01 -0500

    Added NPM Support

---
+++ Scripts/core/game.js
@@ -0,0 +1,15 @@
+// IIFE - Immediate Invoked Function Expression
+/*
+    Closure
+    Calls an anonymous self-executing function
+    Anything in braces is in a closure. Won't go to a global namespace.
+*/
+
+(function () {
+
+    function Init() {
+        console.log("Initialization start")
+    }
+
+    window.onload = Init;
+})();
