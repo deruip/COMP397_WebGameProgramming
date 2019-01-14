4723575cf138b928be7bffda8144e3a7d65cb684
deruip <aronly1997@hotmail.com>
2019-01-14 16:31:10 -0500

    Added TypeScript

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -4,11 +4,14 @@
     Calls an anonymous self-executing function
     Anything in braces is in a closure. Won't go to a global namespace.
 */
+
 (function () {
+
     function Init() {
         console.log("Initialization start");
-        var x = 10;
+
+        let x: number = 10;
     }
+
     window.onload = Init;
 })();
-//# sourceMappingURL=game.js.map
