59c12058836dd24e6a34a640df87c4e85668b822
deruip <aronly1997@hotmail.com>
2019-01-21 18:22:22 -0500

    Added Button Class

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -9,6 +9,7 @@
     var canvas = document.getElementById("canvas");
     var stage;
     var helloLabel;
+    var clickableButton;
     function Init() {
         console.log("Initialization start");
         Start();
@@ -17,6 +18,7 @@
         console.log("Starting Application...");
         // Initialize CreateJS
         stage = new createjs.Stage(canvas);
+        stage.enableMouseOver(20); // Frequency of checks. Computationally expensive function.
         createjs.Ticker.framerate = 60; // 60 FPS
         createjs.Ticker.on("tick", Update);
         Main();
@@ -24,10 +26,28 @@
     function Update() {
         stage.update();
     }
+    /*
+        Button Event Listener Functions
+    */
+    function clickableButtonMouseClick() {
+        helloLabel.text = "Clicked!";
+        helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+        helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+    }
     function Main() {
         console.log("Game Start...");
         helloLabel = new objects.Label("Hello World", "40px", "Consolas", "#000000", 320, 240, true);
+        clickableButton = new objects.Button("./Assets/Sprites/clickablebutton.png", 320, 340);
+        clickableButton.regX = clickableButton.getBounds().width * 0.5;
+        clickableButton.regY = clickableButton.getBounds().height * 0.5;
+        clickableButton.on("click", clickableButtonMouseClick);
+        // clickableButton = new objects.Button("./Assets/Sprites/clickablebutton.png");
+        // clickableButton.x = 320;
+        // clickableButton.y = 340;
+        // clickableButton.on("mouseover", clickableButtonMouseOver);
+        // clickableButton.on("mouseout", clickableButtonMouseOut);
         stage.addChild(helloLabel);
+        stage.addChild(clickableButton);
     }
     window.onload = Init;
 })();
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -11,6 +11,7 @@
     let canvas = document.getElementById("canvas");
     let stage: createjs.Stage;
     let helloLabel: objects.Label;
+    let clickableButton: objects.Button;
 
     function Init(): void {
         console.log("Initialization start");
@@ -24,6 +25,7 @@
 
         // Initialize CreateJS
         stage = new createjs.Stage(canvas);
+        stage.enableMouseOver(20);      // Frequency of checks. Computationally expensive function.
         createjs.Ticker.framerate = 60; // 60 FPS
         createjs.Ticker.on("tick", Update);
         Main();
@@ -33,14 +35,34 @@
         stage.update();
     }
 
+    /*
+        Button Event Listener Functions
+    */
+    function clickableButtonMouseClick(): void {
+        helloLabel.text = "Clicked!";
+        helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+        helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+    }
+
     function Main(): void {
         console.log("Game Start...");
 
         helloLabel = new objects.Label("Hello World", "40px", "Consolas", "#000000", 320, 240, true);
 
+        clickableButton = new objects.Button("./Assets/Sprites/clickablebutton.png", 320, 340);
+        clickableButton.regX = clickableButton.getBounds().width * 0.5;
+        clickableButton.regY = clickableButton.getBounds().height * 0.5;
+        clickableButton.on("click", clickableButtonMouseClick);
 
+        // clickableButton = new objects.Button("./Assets/Sprites/clickablebutton.png");
+        // clickableButton.x = 320;
+        // clickableButton.y = 340;
+        // clickableButton.on("mouseover", clickableButtonMouseOver);
+        // clickableButton.on("mouseout", clickableButtonMouseOut);
+        
 
         stage.addChild(helloLabel);
+        stage.addChild(clickableButton);
     }
 
     window.onload = Init;
